ea6722c799266895a98e6fd78442b86dabccfd8e
Thanh Nguyen <tienthanh8490@gmail.com>
2016-12-30 16:21:05 +0800

    add sample url

--- sublimetunes.py
+++ sublimetunes.py
@@ -1,5 +1,5 @@
 # Reference: https://stackoverflow.com/a/25899180/1442280
 # Use attached vlc file since it's not on pip
 import vlc
-p = vlc.MediaPlayer("westcoast.mp3")
+p = vlc.MediaPlayer("http://www.stephaniequinn.com/Music/Pachelbel%20-%20Canon%20in%20D%20Major.mp3")
 p.play()
